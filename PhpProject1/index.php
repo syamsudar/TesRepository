0edb0e1423df53225dbb2042a2bebc82e6edb251
lanineone <lanineone@lanineone-PC>
2016-01-17 10:16:23 +0700

    Sudar Commit

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,9 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'Add New edited branch Sudar.';
-            
-            
+            echo 'Add New edited from branch Sudar.';
         ?>
     </body>
 </html>
