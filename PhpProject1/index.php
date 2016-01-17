800558ad555469c561b14830c20042a83c01d2cc
lanineone <lanineone@lanineone-PC>
2016-01-17 10:17:41 +0700

    Admin Commit

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'Add New edited from branch Sudar.';
+            echo 'Add New edited from branch Sudar.  ';
         ?>
     </body>
 </html>
